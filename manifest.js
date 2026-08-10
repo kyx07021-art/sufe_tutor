8240327ed4adfaa4501bd0c07cd1ef4f3ba4d974
kyx07021-art <kyx07021@163.com>
2026-08-10 18:38:17 +0800

    v0.26.10 B6 缓存诊断头（排查生产缓存未命中）

--- manifest.js
+++ manifest.js
@@ -9,7 +9,7 @@ export const ASSET_MANIFEST = {
     "style-posts.css": "style-posts.c5d926c9.css",
     "style-chat.css": "style-chat.b764601b.css",
     "glass.css": "glass.0664a6fe.css",
-    "constants.js": "constants.3817f2d2.js",
+    "constants.js": "constants.b2b63de1.js",
     "app-display.js": "app-display.e3cfbdf7.js",
     "app-state.js": "app-state.cb780ba1.js",
     "app-api.js": "app-api.dd8c08f1.js",
