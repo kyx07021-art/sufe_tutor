6b095c6903e2318cac365c1296cf5b69d0f41b99
kyx07021-art <kyx07021@163.com>
2026-08-14 17:37:24 +0800

    v0.31.10: 收口按钮可变宽度版本 bump

--- manifest.js
+++ manifest.js
@@ -9,7 +9,7 @@ export const ASSET_MANIFEST = {
     "style-posts.css": "style-posts.a181f0f2.css",
     "style-chat.css": "style-chat.b77a5019.css",
     "glass.css": "glass.d7dcde4d.css",
-    "constants.js": "constants.897a40bd.js",
+    "constants.js": "constants.790f9cb2.js",
     "app-display.js": "app-display.439b4ea7.js",
     "app-state.js": "app-state.8e0759eb.js",
     "app-api.js": "app-api.d0028a0b.js",
