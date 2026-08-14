3205eda6586586e3cb59b3b13623043cdfcec15e
kyx07021-art <kyx07021@163.com>
2026-08-14 18:07:49 +0800

    v0.31.11: 收口标题字治本版本 bump

--- manifest.js
+++ manifest.js
@@ -9,7 +9,7 @@ export const ASSET_MANIFEST = {
     "style-posts.css": "style-posts.a181f0f2.css",
     "style-chat.css": "style-chat.b77a5019.css",
     "glass.css": "glass.d7dcde4d.css",
-    "constants.js": "constants.790f9cb2.js",
+    "constants.js": "constants.ef9cd50f.js",
     "app-display.js": "app-display.439b4ea7.js",
     "app-state.js": "app-state.8e0759eb.js",
     "app-api.js": "app-api.d0028a0b.js",
